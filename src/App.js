90af41e628e8f266df1a4820407f3643b0b831b8
matthieuVeillon <matthieu.veillon@gmail.com>
2020-07-20 14:54:48 +0200

    remove useEffect and fetch logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const Profile = ({ profile }) => (
@@ -11,32 +11,21 @@ const Profiles = ({ profilesData }) => {
   return (
     <>
       <div>Total Search : {profilesData.total_count}</div>
-      <div>
-        {profilesData.items?.length &&
-          profilesData.items.map(profile => <Profile profile={profile} />)}
-      </div>
+      <div>{profilesData.items?.length && profilesData.items.map(profile => <Profile profile={profile} />)}</div>
     </>
   );
 };
 
+/*
+
+The goal of the exercice is to fetch users and display it from github.
+- when a user enter a profileName this should get the corresponding data from github API and display it with <Profiles> component
+- API to use : https://api.github.com/search/users?q="profileName"
+* */
+
 function App() {
   const [profileName, setProfileName] = useState("tom");
   const [profilesData, setProfilesData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (profileName !== "") {
-        const response = await fetch(
-          `https://api.github.com/search/users?q=${profileName}`
-        );
-
-        const data = await response.json();
-        setProfilesData(data);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleChange = event => {
     event.preventDefault();
@@ -45,12 +34,7 @@ function App() {
 
   return (
     <div className="App">
-      <input
-        placeholder="enter a profile name"
-        value={profileName}
-        type="text"
-        onChange={handleChange}
-      />
+      <input placeholder="enter a profile name" value={profileName} type="text" onChange={handleChange} />
       {profilesData && <Profiles profilesData={profilesData} />}
     </div>
   );
